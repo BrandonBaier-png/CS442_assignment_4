b4aebe91f5b8d3e8d5e7689d2b698eb261df266b
BrandonBaier-png <baierb1283@my.uwstout.edu>
2026-03-21 16:36:45 -0500

    Function piping instances created to satisfy #2) requirements for project, allowing for at least 2 part commands to pass through.

--- Main.cpp
+++ Main.cpp
@@ -32,7 +32,7 @@ using namespace std;
 #include "Main.h"
 #include "history.h"
 // -------------------------------------------------------------- HISTORY -----------------------------------------------------
-
+char* userCommand[4];
 
 // Tokenizes the vector. Useful for later
 vector <string> getTokenVector(const string &userInput) {
@@ -47,14 +47,92 @@ vector <string> getTokenVector(const string &userInput) {
 }
 
 /* this function should be able to run anything that requires a linux command to be run.
+ * 0) determine the desired input & output channel desired for commands to run through this.
+ *          * Assume there will only be one instance of redirection or piping in one command line, handling additional is extra credit
+ *          ** Assume that there is always a space between any redirection / piping operations
  * 1) pipes are created to communicate with the child process
  * 2) process is forked to create an environment for the command to be run
  * 3) parent process waits for child process to run, child process runs the command and writes information to the parent
- * 4) once all finished, parent outputs the information gained to the command line, (in the future this will be able to change with piping & input redirection)
+ * 4) info is output through the correct channel based on piping & redirecting info given
  */
-void pipingInstance() {
+
+// creates the char* array used for
+
+
+// creating a function that
+void pipingInstance(vector<string>& tokenVec) {
+    // parse through the tokenized vector to determine piping or redirection
+    int fd_toChild[2]; // creation of child pipe
+    int fd_toParent[2]; // creation of parent pipe
+
+    // creating piping to child -> parent
+    if(pipe(fd_toChild) == -1){
+        perror("ERROR:Child to Parent Pipe creation failed");
+        exit(2);
+    }
+    // creating piping to parent -> child
+    if(pipe(fd_toParent) == -1){
+        perror("ERROR: Parent to Child Pipe creation failed");
+        exit(3);
+    }
+
+    // creating the userCommand char* to be utilized in child process execvp function
+    int tokenNum = tokenVec.size();
+
+
+    for (int i = 0; i <=tokenVec.size(); i++) {
+        userCommand[i] = (char*) tokenVec[i].c_str();
+    }
+    userCommand[-1] = NULL;
+
+    cout << "ready for piping instance" << endl;
+
+    pid_t child_pid = fork();
+
+    if(child_pid < 0){ // fork failed
+        perror("ERROR: Fork failed");
+    }
+
+    // -------------------------------- CHILD PROCESS --------------------------------
+    if(child_pid == 0){
+        // closing unneeded pipe ends
+        close(fd_toChild[1]);
+        close(fd_toParent[0]);
+
+        // running the linux command. done within the if statement to check if it's run
+        if (execvp(userCommand[0], userCommand) < 0) {
+            cout << "ERROR: couldn't change the execution image for the child process :3" << endl;
+        }
+
+        cout << "This message should never run :3" << endl;
+
+        // choose what input to redirect to here
+        close(fd_toParent[1]);
+
+    }
+    // ------------------------------- PARENT PROCESS --------------------------------
+    else{
+        // closing unnecessary pipe ends
+        close(fd_toChild[0]);
+        close(fd_toParent[1]);
+
+        // sending the child process the user command
+        write(fd_toChild[1], tokenVec[0].c_str(), tokenVec[0].size()+1);
+
+        //closing the child pipe
+        close(fd_toChild[1]);
+
+        // everything before is fine/needs to run before the parent
+        wait(0);
+
+        // output what the child process spits out
+        close(fd_toParent[0]);
+
+
+    }
 
 }
+
 
 
 /* Main contains the main loop of the program, responsible for handling logic involving
@@ -115,6 +193,8 @@ int main() {
 
             cout << "value " << i << ": " << tokenVec[i] << endl;
         }
+
+        pipingInstance(tokenVec);
     }
 
     //write history to the history file given historyVec & standard output save
